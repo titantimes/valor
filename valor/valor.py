a3deb053a9e9df065c4e7c69ffb0f21d225d6f2e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 21:41:40 -0800

    new file:   commands/__init__.py 	new file:   commands/online.py 	new file:   config.json 	new file:   main.py 	new file:   valor/__init__.py 	new file:   valor/valor.py

---
+++ valor/valor.py
@@ -0,0 +1,30 @@
+import discord
+import os
+import json
+
+
+class Valor(discord.ext.commands.Bot):
+    """
+    Subclass of discord.ext.commands.Bot class holding additional information
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        self.BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+        self.config = {
+            "description": "A bot to help out with guild tasks."
+        }
+
+        # default configuration file is not found
+        if not os.path.exists('config.json'):
+            with open("config.json", 'w') as f:
+                json.dump(self.config, f)
+        else:
+            with open("config.json", 'r') as f:
+                self.config = json.load(f)
+
+        super(Valor, self).__init__(*args, **kwargs, description=self.config["description"])
+
+    def run(self):
+        super(Valor, self).run(self.BOT_TOKEN)
