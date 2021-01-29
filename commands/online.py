01e1597f6f1ca3962d9beca4107498d6f255e2d4
classAndrew <andrxwlin@gmail.com>
2021-01-28 18:19:24 -0800

    Add wynnbuilder embed, Fielded embeds

--- commands/online.py
+++ commands/online.py
@@ -5,9 +5,7 @@ import random
 import requests
 
 async def _register_online(valor: Valor):
-    """
-    Online: shows who's online in a guild
-    """
+    desc = "Online: shows who's online in a guild"
     @valor.command()
     async def online(ctx: Context, guild="Titans Valor"):
         res = requests.get(valor.endpoints["guild"].format(guild)).json()
@@ -18,6 +16,10 @@ async def _register_online(valor: Valor):
         del all_players["request"]
         online_rn = [(p, k) for k in all_players for p in all_players[k] if p in members]
         await ctx.send("```"+'\n'.join("%16s | %8s" % (p, k) for p, k in online_rn) + "```")
+    
+    @valor.help_override.command()
+    async def online(ctx: Context):
+        await ctx.send(desc)
     
     
     
--- valor/valor.py
+++ valor/valor.py
@@ -11,6 +11,8 @@ class Valor(discord.ext.commands.Bot):
     def __init__(self, *args, **kwargs):
 
         self.BOT_TOKEN = os.environ["BOT_TOKEN"]
+        # this is used to override the help command and then register the help subcommands for other commands
+        self.help_override = None
 
         self.config = {
             "description": "A bot to help out with guild tasks."
