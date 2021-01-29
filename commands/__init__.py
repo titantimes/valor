377f03d3fc9e1e4def3346cb8f247173983a7257
classAndrew <andrxwlin@gmail.com>
2021-01-28 18:19:24 -0800

    Add wynnbuilder embed, Fielded embeds

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,12 +1,14 @@
 from .online import _register_online
 from .test import _register_test
 from .help import _register_help
+from .showbuild import _register_showbuild
 from valor import Valor
 
 async def register_all(valor: Valor):
     """
     Registers all the commands"
     """
+    await _register_help(valor)
     await _register_online(valor)
     await _register_test(valor)
-    await _register_help(valor)
+    await _register_showbuild(valor)
