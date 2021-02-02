44d627a3ebf49258bd23f0ab86555893e7f173dd
classAndrew <andrxwlin@gmail.com>
2021-02-02 13:11:45 -0800

    Fixed: preference changes

--- commands/preferences.py
+++ commands/preferences.py
@@ -3,8 +3,8 @@ from discord.ext.commands import Context
 from util import ErrorEmbed, LongTextEmbed, info, LongFieldEmbed
 from sql import ValorSQL
 
-preferences = {"wynnbuilder"}
-pref_list = list(preferences)
+pref_list = ["wynnbuilder"]
+pref_set = set(pref_list)
 
 async def _register_preferences(valor: Valor):
     desc = """
@@ -20,15 +20,17 @@ async def _register_preferences(valor: Valor):
 
     @preferences.command()
     async def enable(ctx: Context, preference_name: str):
-        if not preference_name in preferences:
+        if not preference_name in pref_set:
             return await ctx.send(embed=ErrorEmbed("Preference not found. -preference for a list of preferences"))
         ValorSQL.set_user_wynnbuilder(ctx.author.id, preference_name, True)
+        await LongTextEmbed.send_message(valor, ctx, "Enabled preference")
     
     @preferences.command()
     async def disable(ctx: Context, preference_name: str):
-        if not preference_name in preferences:
+        if not preference_name in pref_set:
             return await ctx.send(embed=ErrorEmbed("Preference not found. -preference for a list of preferences"))
         ValorSQL.set_user_wynnbuilder(ctx.author.id, preference_name, False)
+        await LongTextEmbed.send_message(valor, ctx, "Disabled preference")
     
     @preferences.error
     async def cmd_error(ctx, error):
