8254b69471d8eebc0866517adb5817e1527a6179
classAndrew <andrxwlin@gmail.com>
2021-02-02 12:45:51 -0800

    Fix: 0-division, one page no reaction, Changed: online no spaces

--- commands/online.py
+++ commands/online.py
@@ -7,7 +7,8 @@ import requests
 async def _register_online(valor: Valor):
     desc = "Online: shows who's online in a guild"
     @valor.command()
-    async def online(ctx: Context, guild="Titans Valor"):
+    async def online(ctx: Context, *args, guild="Titans Valor"):
+        guild = ' '.join(args) if len(args) else guild
         res = requests.get(valor.endpoints["guild"].format(guild)).json()
         if res.get("error"):
             return await ctx.send(embed=ErrorEmbed("Guild doesn't exist."))
@@ -16,8 +17,8 @@ async def _register_online(valor: Valor):
         del all_players["request"]
         online_rn = [(p, k) for k in all_players for p in all_players[k] if p in members]
         if not len(online_rn):
-            return await LongTextEmbed.send_message(valor, ctx, "Guild Members Online", "There are no members online.", color = 0xFF)
-        await LongFieldEmbed.send_message(valor, ctx, "Guild Members Online", online_rn)
+            return await LongTextEmbed.send_message(valor, ctx, f"{guild} Members Online", "There are no members online.", color = 0xFF)
+        await LongFieldEmbed.send_message(valor, ctx, f"{guild} Members Online", online_rn)
         # await ctx.send("```"+'\n'.join("%16s | %8s" % (p, k) for p, k in online_rn) + "```")
     
     @online.error
