cbc8009d92b358c146b9fd52355c357068d6ae8c
classAndrew <andrxwlin@gmail.com>
2021-02-04 11:42:05 -0800

    Add: Logging

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ import time
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.getLogger().setLevel(logging.INFO)
+logging.warning("Starting")
 valor = valor.Valor('-')
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(commands.register_all(valor))
 loop.run_until_complete(listeners.register_all(valor))
+
 async def check():
     while True:
-        logging.info("Time")
+        logging.info("Ping")
         await asyncio.sleep(60)
-loop.run_until_complete(check)
+loop.create_task(check())
+
 valor.run()
