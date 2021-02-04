cbfbf1fbd676d41a477f304ab24737f1fcb739a7
classAndrew <andrxwlin@gmail.com>
2021-02-04 11:23:42 -0800

    Changed: main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import valor
 import commands
 import listeners
 import asyncio
+import logging
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,5 +16,9 @@ valor = valor.Valor('-')
 loop = asyncio.get_event_loop()
 loop.run_until_complete(commands.register_all(valor))
 loop.run_until_complete(listeners.register_all(valor))
-
+async def check():
+    while True:
+        logging.info("Time")
+        await asyncio.sleep(60)
+loop.run_until_complete(check)
 valor.run()
