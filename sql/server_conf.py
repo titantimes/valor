f0faf872c37c58d9039b2fdcc4c0a323bcceca7f
classAndrew <andrxwlin@gmail.com>
2021-02-06 12:30:03 -0800

    Debug: SQL test

--- sql/server_conf.py
+++ sql/server_conf.py
@@ -1,5 +1,6 @@
 # Connecting to SQL DB for handling server configurations
 import mysql.connector
+from mysql.connector import pooling
 import os
 from dotenv import load_dotenv
 # in case it hasn't already been done yet
@@ -10,10 +11,7 @@ class ValorSQL:
         "user": os.environ["DBUSER"],
         "password": os.environ["DBPASS"],
         "database":os.environ["DBNAME"]}
-    db = mysql.connector.connect(
-        **_info
-    )
-    cursor = db.cursor()
+    pool = pooling.MySQLConnectionPool(pool_name="valor_pool", **_info, pool_size=1)
     @classmethod
     def insert_new_server(cls, *args):
         # get columns N before string indicates unicode
@@ -23,14 +21,12 @@ class ValorSQL:
             raise Exception("DB QUERY: Column Mismatch")
         cls._execute(
             f"INSERT INTO server_options ({', '.join(map(str, cols))}) VALUES ({', '.join(map(str, args))})")
-        cls.db.commit()
     
     # should probably use a decorator for the ones with _add_new_user
     @classmethod
     def set_user_wynnbuilder(cls, userid: int, pref: str, r: bool):
         cls._add_new_user(userid)
         cls._execute(f"UPDATE user_config SET {pref} = {r} WHERE user_id = {userid}")
-        cls.db.commit()
 
     @classmethod
     def get_user_config(cls, userid: int):
@@ -40,6 +36,7 @@ class ValorSQL:
         res = cls._execute(f"SELECT * FROM user_config WHERE user_id = {userid}")
         res = res[0]
         return {cols[i]: res[i] for i in range(len(cols))}
+
     @classmethod
     def _add_new_user(cls, userid: int):
         res = cls._execute(f"SELECT * FROM user_config WHERE user_id = {userid}")
@@ -48,30 +45,28 @@ class ValorSQL:
         res = cls._execute("SHOW columns FROM user_config")
         cols = tuple(c[0] for c in res)
         cls._execute(f"INSERT INTO user_config (user_id) VALUES ({userid})")
-        cls.db.commit()
 
     @classmethod
     def _execute(cls, query: str):
-        try:
-            cls.db.ping(reconnect=True, attempts=10, delay=30)
-        except Exception as e:
-            print(e)
-            cls._reconnect()
-        # refrsehing the cursor is very important see _fetchall for closing the cursor
-        cursor = cls.db.cursor()
+        conn = cls.pool.get_connection()
+        while not conn.is_connected():
+            print("Not connected")
+            conn = cls.pool.get_connection()
+        cursor = conn.cursor()
         cursor.execute(query)
         res = list(cursor.fetchall())
+        conn.commit()
         cursor.close()
         return res
 
-    @classmethod
-    def _reconnect(cls):
-        print("ValorSQL: Reconnecting to db")
-        cls.cursor.close()
-        cls.db.close()
-        cls.db = mysql.connector.connect(
-            **cls._info
-        )
+    # @classmethod
+    # def _reconnect(cls):
+    #     print("ValorSQL: Reconnecting to db")
+    #     cls.cursor.close()
+    #     cls.db.close()
+    #     cls.db = mysql.connector.connect(
+    #         **cls._info
+    #     )
 if __name__ == "__main__":
     # ValorSQL.insert_new_server(1, 1)
     ValorSQL._add_new_user(1651)
