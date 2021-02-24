e38f8c26244d8832126f84dc6cca61061abf64fc
classAndrew <andrxwlin@gmail.com>
2021-02-23 18:47:53 -0800

    Fixed: Time update bug

--- commands/plot.py
+++ commands/plot.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import time
 import math
+import random
 
 load_dotenv()
 async def _register_plot(valor: Valor):
@@ -22,9 +23,12 @@ async def _register_plot(valor: Valor):
             await ctx.send(embed=choice_em)
     
     @plot.command()
-    async def guild(ctx: Context, guild_name = "Avicia", start=int(time.time())-7*24*3600, end=int(time.time())):
+    async def guild(ctx: Context, guild_name = "Avicia", start = None, end= None):
+        if not end:
+            end = int(time.time())
+        if not start:
+            start = int(time.time()) - 3600*24*7
         fig: plt.Figure = plt.figure()
-        fig.clear()
         ax: plt.Axes = fig.add_subplot(5,1,(1,4))
         ax.set_ylabel("Player Online Count")
         ax.set_xlabel("Date (by the hour)")
@@ -32,22 +36,19 @@ async def _register_plot(valor: Valor):
         schema = "https://" if os.getenv("USESSL") == "true" else "http://"
         # print(schema+os.getenv("REMOTE")+os.getenv("RMPORT")+f"/activity/guild/{guild_name}/{start}/{end}")
         res = requests.get(schema+os.getenv("REMOTE")+os.getenv("RMPORT")+f"/activity/guild/{guild_name}/{start}/{end}").json()
-        old_xvalues = list(res["data"])
+        old_xvalues = [*map(int, res["data"])]
         # old_xvalues = sorted(old_xvalues)
-        xvalues = [datetime.fromtimestamp(int(old_xvalues[0])).strftime("%-d/%m/%y-%H")]
-        yvalues = [res["data"][old_xvalues[0]]]
+        xvalues = [datetime.fromtimestamp(old_xvalues[0]).strftime("%-d/%m/%y-%H")]
+        yvalues = [res["data"][str(old_xvalues[0])]]
         for i in range(1, len(old_xvalues)):
             # fill in the gaps in time
-            old_xvalues[i] = int(old_xvalues[i])
-            old_xvalues[i-1] = int(old_xvalues[i-1]) 
-            fill = (old_xvalues[i]-old_xvalues[i-1])/3600
+            fill = (old_xvalues[i]-old_xvalues[i-1])//3600
             if fill > 2:
-                fill = int(fill)
                 xvalues.extend(
                     [datetime.fromtimestamp(old_xvalues[i-1]+j*3600).strftime("%-d/%m/%y-%H")
                         for j in range(1, fill)]
                 )
-                yvalues.extend([0 for _ in range(1, fill)])
+                yvalues.extend([0]*(fill-1))
             xvalues.append(datetime.fromtimestamp(old_xvalues[i]).strftime("%-d/%m/%y-%H"))
             yvalues.append(res["data"][str(old_xvalues[i])])
         ax.plot(xvalues, yvalues)
