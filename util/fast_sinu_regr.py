5c5c3d59fb93af9b25ef9ae2e3ca57d3115a1247
classAndrew <andrxwlin@gmail.com>
2021-03-03 16:10:18 -0800

    Added: sinusoidal regression

---
+++ util/fast_sinu_regr.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple
+import math
+import numpy as np
+def sinusoid_regress(x, y) -> Tuple[int, int, int, int]:
+    avg = sum(y)/len(y)
+    stdev = (sum((yi-avg)**2 for yi in y)/len(y))**.5
+    # to find period, clump the points together at amplitude intersection
+    E = 1
+    inx = [x[i] for i in range(len(x)) if abs(y[i]-avg+stdev) < E]
+    # print('\n'.join(f"{x}\t4" for x in inx))
+
+    min_period = 40000
+    periods = [inx[i]-inx[i-1] for i in range(len(inx)) if inx[i]-inx[i-1] >= min_period]
+    avg_period = np.mean(periods)
+    freq = 1/avg_period
+    print(avg, stdev, freq*2*3.1415)
+    # candidate phase shifts
+    clamp = lambda x: -1 if x < -1 else 1 if x > 1 else x
+    candidate_ps = [math.asin(clamp((y[i]-avg)/stdev))-(freq*2*3.1415)*x[i] for i in range(len(x))]
+
+    def model(x, c):
+        return stdev*math.sin(avg_period*x-c)+avg
+
+    def r_squared(model, c):
+        error = sum((model(x[i], c)-y[i])**2 for i in range(len(x)))
+        return error
+    # a*sin(bx-c)+d
+    return stdev, avg_period, max(candidate_ps, key = lambda c: r_squared(model, c)), avg
