90a4a634c63359c4e6978063fee4704bb668e7b4
classAndrew <andrxwlin@gmail.com>
2021-03-06 18:12:49 -0800

    Added: guild tags

---
+++ util/guild_tags.py
@@ -0,0 +1,10 @@
+from functools import lru_cache
+import requests
+
+dat = requests.get("https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=guild&timeframe=alltime").json()
+@lru_cache(500)
+def guild_name_from_tag(tag: str) -> str:
+    for k in dat["data"]:
+        if k["prefix"] == tag:
+            return k["name"]
+    return ''
