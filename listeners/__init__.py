ce11bfe37c47af439b752c733726add5b69d8332
classAndrew <andrxwlin@gmail.com>
2021-04-05 14:32:35 -0700

    Remove: embed build

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -2,4 +2,6 @@ from .msg_listener import _register_msg_listiner
 from valor import Valor
 
 async def register_all(valor: Valor):
-    await _register_msg_listiner(valor)
+    # people didn't want this
+    # await _register_msg_listiner(valor)
+    return
