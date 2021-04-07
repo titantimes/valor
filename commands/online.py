bff52373b45d87986070483c21ed829ffb566d45
classAndrew <andrxwlin@gmail.com>
2021-04-06 19:30:34 -0700

    Added: Online member improvements

--- commands/online.py
+++ commands/online.py
@@ -1,21 +1,37 @@
 from valor import Valor
 from discord.ext.commands import Context
-from util import ErrorEmbed, LongTextEmbed, LongFieldEmbed
+from util import ErrorEmbed, LongTextEmbed, LongFieldEmbed, guild_name_from_tag
 import random
 import requests
 
 async def _register_online(valor: Valor):
     desc = "Online: shows who's online in a guild"
+    rank_order = dict(zip(["RECRUIT", "RECRUITER", "CAPTAIN", "STRATEGIST", "CHIEF", "OWNER"], range(5, -1, -1)))
     @valor.command()
     async def online(ctx: Context, *args, guild="Titans Valor"):
         guild = ' '.join(args) if len(args) else guild
         res = requests.get(valor.endpoints["guild"].format(guild)).json()
+
+        if res.get("error"):
+            # try using the tag instead
+            guild = guild_name_from_tag(guild)
+
+        res = requests.get(valor.endpoints["guild"].format(guild)).json()
+
         if res.get("error"):
             return await ctx.send(embed=ErrorEmbed("Guild doesn't exist."))
-        members = {m["name"] for m in res["members"]}
+
+        members = {m["name"]: m["rank"] for m in res["members"]}
         all_players = requests.get(valor.endpoints["online"]).json()
+
         del all_players["request"]
-        online_rn = [(p, k) for k in all_players for p in all_players[k] if p in members]
+
+        # add the rank name along with wc
+        online_rn = [(p, k, members[p]) for k in all_players for p in all_players[k] if p in members]
+        online_rn.sort(key = lambda x: rank_order[x[2]])
+        
+        # escape any underscores and concat rank with world number
+        online_rn = [(p.replace("_", "\_"), f"{k}\n{rank}") for p, k, rank in online_rn]
         if not len(online_rn):
             return await LongTextEmbed.send_message(valor, ctx, f"{guild} Members Online", "There are no members online.", color = 0xFF)
         await LongFieldEmbed.send_message(valor, ctx, f"{guild} Members Online ({len(online_rn)})", online_rn)
