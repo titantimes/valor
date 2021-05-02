a724e90ce34ba824c3392ca0e68e9325d6e7b26b
classAndrew <andrxwlin@gmail.com>
2021-05-02 12:48:48 -0700

    Fix: floor'd seconds division

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -1,6 +1,8 @@
 from .msg_listener import _register_msg_listiner
+from .react_listener import _register_react_listener
 from valor import Valor
 
 async def register_all(valor: Valor):
     await _register_msg_listiner(valor)
+    await _register_react_listener(valor)
     return
--- listeners/react_listener.py
+++ listeners/react_listener.py
@@ -1,3 +1,23 @@
 from valor import Valor
 import discord
 from util import ErrorEmbed, LongTextEmbed, info, LongFieldEmbed
+import time
+from sql import ValorSQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def _register_react_listener(valor: Valor):
+    @valor.event
+    async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+        # REQUIRES MEMBER INTENT TO BE ENABLED IN DISCORD SETTINGS
+        msg_id = payload.message_id
+        if payload.user_id != int(os.environ["SELFID"]) and valor.reaction_msg_ids.get(msg_id, 0) > int(time.time()):
+            emoji = str(payload.emoji)
+            if len(emoji) == 1:
+                emoji = ord(emoji)
+            else:
+                emoji = int(emoji.split(":")[-1][:-1])
+            res = ValorSQL.get_react_msg_reaction(msg_id,  emoji)
+            print(res)
