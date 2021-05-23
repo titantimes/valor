365939d20c5866d36e17899d45a41bbd6098d33c
classAndrew <andrxwlin@gmail.com>
2021-05-22 20:00:50 -0700

    Add online wc group

--- commands/online.py
+++ commands/online.py
@@ -9,6 +9,10 @@ async def _register_online(valor: Valor):
     rank_order = dict(zip(["RECRUIT", "RECRUITER", "CAPTAIN", "STRATEGIST", "CHIEF", "OWNER"], range(5, -1, -1)))
     @valor.command()
     async def online(ctx: Context, *args, guild="Titans Valor"):
+        group = False
+        if args and args[0] == 'g':
+            args = args[1:]
+            group = True
         guild = ' '.join(args) if len(args) else guild
         res = requests.get(valor.endpoints["guild"].format(guild)).json()
 
@@ -28,12 +32,24 @@ async def _register_online(valor: Valor):
 
         # add the rank name along with wc
         online_rn = [(p, k, members[p]) for k in all_players for p in all_players[k] if p in members]
-        online_rn.sort(key = lambda x: rank_order[x[2]])
         
-        # escape any underscores and concat rank with world number
-        online_rn = [(p.replace("_", "\_"), f"{k}\n{rank}") for p, k, rank in online_rn]
         if not len(online_rn):
             return await LongTextEmbed.send_message(valor, ctx, f"{guild} Members Online", "There are no members online.", color = 0xFF)
+        if group:
+            grouped = {}
+            for p, k, rank in online_rn:
+                if not k in grouped:
+                    grouped[k] = []
+                grouped[k].append((p, k, rank))
+            grouped = [x[1] for x in sorted(grouped.items(), key=lambda v: len(v[1]), reverse=True)]
+            return await ctx.send(('```'+'%38s' % f"Members of {guild} online\n") +
+                '-'*17+'+'+'-'*7+'+'+'-'*12+'\n'+
+                '\n'.join('\n'.join(('%16s | %5s | %10s' % x) for x in y)+'\n' for y in grouped) + '```'    
+            )
+        online_rn.sort(key = lambda x: rank_order[x[2]])
+        # escape any underscores and concat rank with world number
+        online_rn = [(p.replace("_", "\_"), f"{k}\n{rank}") for p, k, rank in online_rn]
+
         await LongFieldEmbed.send_message(valor, ctx, f"{guild} Members Online ({len(online_rn)})", online_rn)
         # await ctx.send("```"+'\n'.join("%16s | %8s" % (p, k) for p, k in online_rn) + "```")
     
