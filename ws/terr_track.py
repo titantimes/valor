c78c0ff82a1c0d63651f9c483477eafad1b238da
classAndrew <andrxwlin@gmail.com>
2021-06-13 10:43:14 -0700

    Fix parse error

--- ws/terr_track.py
+++ ws/terr_track.py
@@ -38,14 +38,15 @@ async def _register_terr_track(valor: Valor):
             try:
                 async for msg in c:
                     msg = json.loads(msg)
-                    if msg.get("defender") == "Titans Valor" and msg.get("territory") in terrs:
-                        for cid in chn_ids:
-                            chn = valor.get_channel(cid)
-                            if chn and time.time()-last_pinged >= 3600*2:
-                                await chn.send_message(embed=LongTextEmbed("Ping! We're under attack!", 
-                                f"Attacker: **{msg['attacker']}**\nTerritory: **{msg['territory']}**",
-                                color=0xFF2222))
-                                last_pinged = time.time()
+                    for action in msg:
+                        if action.get("defender") == "Titans Valor" and action.get("territory") in terrs:
+                            for cid in chn_ids:
+                                chn = valor.get_channel(cid)
+                                if chn and time.time()-last_pinged >= 3600*2:
+                                    await chn.send_message(embed=LongTextEmbed("Ping! We're under attack!", 
+                                    f"Attacker: **{action['attacker']}**\nTerritory: **{action['territory']}**",
+                                    color=0xFF2222))
+                                    last_pinged = time.time()
             except Exception as e:
                 await asyncio.sleep(30)
                 await c.close()
