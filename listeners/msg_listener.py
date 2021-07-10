44b2a7fc75650a9d8412abd9a9ffb87b9ed032c4
classAndrew <andrxwlin@gmail.com>
2021-07-10 12:29:23 -0700

    remove wynnbuilder embed substitute code

--- listeners/msg_listener.py
+++ listeners/msg_listener.py
@@ -11,32 +11,32 @@ async def _register_msg_listiner(valor: Valor):
     cooldown = {}
     @valor.event
     async def on_message(message: discord.Message):
-        if "https://wynnbuilder.github.io/#" in message.content or \
-            "https://hppeng-wynn.github.io/#" in message.content:
-            # Disable this feature. It's no longer wanted
-            return
+        # if "https://wynnbuilder.github.io/#" in message.content or \
+        #     "https://hppeng-wynn.github.io/#" in message.content:
+        #     # Disable this feature. It's no longer wanted
+        #     return
 
-            usr_conf = ValorSQL.get_user_config(message.author.id)
-            if not usr_conf.get("wynnbuilder"):
-                return
-            link = message.content
-            pos = link.find(".github.io/#")+len(".github.io/#")-1
-            ending = link[pos:].find(' ')
-            # make sure to also handle newlines
-            a = link[pos:].find('\n')
-            if a != -1 and a < ending:
-                ending = a
-            link = link[pos:][:ending]
-            build = info(link)
-            ctx = await valor.get_context(message)
-            return await LongFieldEmbed.send_message(valor, ctx, "WynnBuilder build", build)
-        if len(message.content) and message.content[0] == '-':
-            now = time.time()
-            ctx = await valor.get_context(message)
-            if now-cooldown.get(message.author.id, 0) <= 2:
-                await ctx.send(embed=ErrorEmbed("Please don't spam commands :). The cooldown is 2 seconds"))
-            else:
-                cooldown[message.author.id] = now
+        #     usr_conf = ValorSQL.get_user_config(message.author.id)
+        #     if not usr_conf.get("wynnbuilder"):
+        #         return
+        #     link = message.content
+        #     pos = link.find(".github.io/#")+len(".github.io/#")-1
+        #     ending = link[pos:].find(' ')
+        #     # make sure to also handle newlines
+        #     a = link[pos:].find('\n')
+        #     if a != -1 and a < ending:
+        #         ending = a
+        #     link = link[pos:][:ending]
+        #     build = info(link)
+        #     ctx = await valor.get_context(message)
+        #     return await LongFieldEmbed.send_message(valor, ctx, "WynnBuilder build", build)
+        # if len(message.content) and message.content[0] == '-':
+        #     now = time.time()
+        #     ctx = await valor.get_context(message)
+        #     if now-cooldown.get(message.author.id, 0) <= 2:
+        #         await ctx.send(embed=ErrorEmbed("Please don't spam commands :). The cooldown is 2 seconds"))
+        #     else:
+        #         cooldown[message.author.id] = now
 
         # for application processing
         if message.author.id != int(os.environ["SELFID"]) and (message.channel.name.startswith("app") or message.channel.name.startswith("cpt") or
