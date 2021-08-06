a564ecfba91c9bdcd09e62b934f40c9c93cc42c6
classAndrew <andrxwlin@gmail.com>
2021-08-05 18:05:07 -0700

    add zero to xp rank

--- util/profile_calc.py
+++ util/profile_calc.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 war_ranks = [0, 20, 200, 800, 2000, 2700, 3500, 5000]
-xp_ranks = [10e6, 50e6, 500e6, 1e9, 2.5e9, 5e9, 10e9]
+xp_ranks = [0, 10e6, 50e6, 500e6, 1e9, 2.5e9, 5e9, 10e9]
 numeral_map = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 # returns (rank, next_count)
 def get_war_rank(warcnt: int) -> Tuple[int, int]:
