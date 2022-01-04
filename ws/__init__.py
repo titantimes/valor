d92eaaa9adb5682fe1afff59bb24015cdb432e01
classAndrew <classAndrew@users.noreply.github.com>
2022-01-03 23:17:20 -0800

    add join leave messages

--- ws/__init__.py
+++ ws/__init__.py
@@ -1,5 +1,7 @@
 from .terr_track import _register_terr_track
+from .join_leave import _register_join_leave
 from valor import Valor
 
 async def register_all(valor: Valor):
-    await _register_terr_track(valor)
+    # await _register_terr_track(valor)
+    await _register_join_leave(valor)
