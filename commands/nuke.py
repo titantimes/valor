859503de71ab7befa76ebc5394d3e040a2a5dbcc
classAndrew <classAndrew@users.noreply.github.com>
2022-04-08 21:20:32 -0700

    role nuke

--- commands/nuke.py
+++ commands/nuke.py
@@ -10,13 +10,21 @@ from datetime import datetime
 import requests
 
 async def _register_nuke(valor: Valor):
-    desc = "Removes the roles of certain players"
+    desc = "Entire guild changes"
     
     @valor.command()
     async def nuke(ctx: Context):
         if not ctx.author.guild_permissions.administrator:
             await LongTextEmbed(valor, ctx, "Nuke", "you probably want to stay away from this function...", color=0XFF0000)
         
+        for member in ctx.guild.members:
+            rls = {x.id for x in member.roles}
+            name = member.name.split(" ")
+            if 892886015017103360 in rls:
+                await member.edit(nick="Sorcerer "+name[1])
+            elif 702996152982962197 in rls:
+                await member.edit(nick="Magi "+name[1])
+
 
     @nuke.error
     async def cmd_error(ctx, error: Exception):
