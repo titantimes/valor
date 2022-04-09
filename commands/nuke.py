9da7eb2a318a4e7c458a9720bed001bdfa2cad43
classAndrew <classAndrew@users.noreply.github.com>
2022-04-08 21:49:46 -0700

    remove nuke nickname changes

--- commands/nuke.py
+++ commands/nuke.py
@@ -18,18 +18,18 @@ async def _register_nuke(valor: Valor):
         if not ctx.author.guild_permissions.administrator and ctx.author.id != 146483065223512064:
             await LongTextEmbed(valor, ctx, "Nuke", "you probably want to stay away from this function...", color=0XFF0000)
         
-        for member in ctx.guild.members:
-            rls = {x.id for x in member.roles}
-            name = member.display_name.split(" ")
-            print(name)
-            try:
-                name = name[0] if len(name) == 1 else name[1]
-                if 892886015017103360 in rls:
-                    await member.edit(nick="Sorcerer "+name)
-                elif 702996152982962197 in rls:
-                    await member.edit(nick="Magi "+name)
-            except:
-                print("fail on", name, member.id)
+        # for member in ctx.guild.members:
+        #     rls = {x.id for x in member.roles}
+        #     name = member.display_name.split(" ")
+        #     print(name)
+        #     try:
+        #         name = name[0] if len(name) == 1 else name[1]
+        #         if 892886015017103360 in rls:
+        #             await member.edit(nick="Sorcerer "+name)
+        #         elif 702996152982962197 in rls:
+        #             await member.edit(nick="Magi "+name)
+        #     except:
+        #         print("fail on", name, member.id)
 
 
     @nuke.error
