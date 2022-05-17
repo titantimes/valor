31d75f8a6013c5b2e9e426bfc90ce1e6e98f272c
classAndrew <classAndrew@users.noreply.github.com>
2022-05-16 19:00:31 -0700

    add healer, dps, guard pings

--- commands/ffa.py
+++ commands/ffa.py
@@ -16,10 +16,35 @@ async def _register_ffa(valor: Valor):
     @valor.command()
     async def ffa(ctx: Context):
         if not commands.common.role1(ctx.author, allow={536068288606896128}) and not TEST:
-            return await ctx.send(embed=ErrorEmbed("No Permissions. (you need military role"))
+            return await ctx.send(embed=ErrorEmbed("No Permissions. (you need military role)"))
     
         await ctx.send("<@&892884695199666187>", 
             embed=LongTextEmbed(title="FFA Ping", content="<insert whatever the ffa copypasta is rn> to War!!! \nI'm using some insane algorithm to predict that Aiza is pinging this at 3am", color=0xFF8888))
+    
+    # other war pings
+    @valor.command()
+    async def dps(ctx: Context):
+        if not commands.common.role1(ctx.author, allow={536068288606896128}) and not TEST:
+            return await ctx.send(embed=ErrorEmbed("No Permissions. (you need military role)"))
+    
+        await ctx.send("<@&892885182300954624>", 
+            embed=LongTextEmbed(title="DPS Ping", content="DPS (damage per second) needed", color=0xFF8888))
+    
+    @valor.command()
+    async def guardian(ctx: Context):
+        if not commands.common.role1(ctx.author, allow={536068288606896128}) and not TEST:
+            return await ctx.send(embed=ErrorEmbed("No Permissions. (you need military role)"))
+    
+        await ctx.send("<@&892884953996591164>", 
+            embed=LongTextEmbed(title="Guardian Ping", content="Guardian needed", color=0xFF8888))
+    
+    @valor.command()
+    async def healer(ctx: Context):
+        if not commands.common.role1(ctx.author, allow={536068288606896128}) and not TEST:
+            return await ctx.send(embed=ErrorEmbed("No Permissions. (you need military role)"))
+    
+        await ctx.send("<@&89288538174432053>", 
+            embed=LongTextEmbed(title="Healer Ping", content="Healer needed", color=0xFF8888))
     
 
     @valor.help_override.command()
