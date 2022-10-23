8751b2a3c18c03b607ee5a34d08e1d46f72bd547
RawPikachu <102355977z@gmail.com>
2022-10-23 16:21:04 -0400

    Revert "make main.py asynchronous"
    This reverts commit d14a3cbe8c783845e260bf9457e5b2b9313fdc5e.

--- main.py
+++ main.py
@@ -23,17 +23,15 @@ logging.basicConfig(format='%(asctime)s %(message)s')
 logging.getLogger().setLevel(logging.INFO)
 logging.warning("Starting")
 
-async def main():
-    async with valor:
-        loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
 
-        valor = valor.Valor('-', intents=discord.Intents.all())
+valor = valor.Valor('-', intents=discord.Intents.all())
 
-        ValorSQL.pool = valor.loop.run_until_complete(aiomysql.create_pool(**ValorSQL._info, loop=valor.loop))
+ValorSQL.pool = valor.loop.run_until_complete(aiomysql.create_pool(**ValorSQL._info, loop=valor.loop))
 
-        loop.run_until_complete(commands.register_all(valor))
-        loop.run_until_complete(listeners.register_all(valor))
-        loop.run_until_complete(ws.register_all(valor))
-        # loop.run_until_complete(cron._smp_loop(valor))
+loop.run_until_complete(commands.register_all(valor))
+loop.run_until_complete(listeners.register_all(valor))
+loop.run_until_complete(ws.register_all(valor))
+# loop.run_until_complete(cron._smp_loop(valor))
 
-        valor.run()
+valor.run()
