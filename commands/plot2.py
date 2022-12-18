0b7e6981506a7443321f996970b2999e6288d63c
classAndrew <classAndrew@users.noreply.github.com>
2022-12-17 18:38:16 -0800

    remove gl roles for plot2

--- commands/plot2.py
+++ commands/plot2.py
@@ -28,8 +28,8 @@ async def _register_plot2(valor: Valor):
     @valor.command()
     async def plot2(ctx: Context, *options):
         roles = {x.id for x in ctx.author.roles}
-        if not 703018636301828246 in roles and not 733841716855046205 in roles and ctx.author.id != 146483065223512064:
-            return await ctx.send(embed=ErrorEmbed("Skill Issue"))
+        # if not 703018636301828246 in roles and not 733841716855046205 in roles and ctx.author.id != 146483065223512064:
+        #     return await ctx.send(embed=ErrorEmbed("Skill Issue"))
         
         try:
             opt = parser.parse_args(options)
