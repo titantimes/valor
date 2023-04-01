0cdab32a2489e778d12e50d9885423934dd5a2f0
classAndrew <classAndrew@users.noreply.github.com>
2023-04-01 09:48:14 -0700

    add threshold range to wipe time

--- commands/wipe.py
+++ commands/wipe.py
@@ -23,8 +23,8 @@ async def _register_wipe(valor: Valor):
     wipe_parser = argparse.ArgumentParser(description='Wipe Time Command')
     wipe_parser.add_argument('-g', '--guild', nargs='+', default=["ANO"])
     wipe_parser.add_argument('-r', '--range', nargs='+', default=[7*24, 0])
-    wipe_parser.add_argument('-t', '--threshold', type=int, default=1)
-    wipe_parser.add_argument('-m', '--minsec', type=int, default=120)
+    wipe_parser.add_argument('-t', '--threshold', nargs='+', default=[1, 0])
+    wipe_parser.add_argument('-m', '--minsec', type=float, default=120)
     wipe_parser.add_argument('-s', '--sort', choices=["ffa", "reclaim", "help", "other", "total"], default="total")
 
     @valor.command()
@@ -37,7 +37,7 @@ async def _register_wipe(valor: Valor):
         start = time.time()
         g_names = {await guild_name_from_tag(tag) for tag in opt.guild}
         g_names_paren = [f"'{n}'" for n in g_names]
-        left, right = start - int(opt.range[0])*24*3600, start - int(opt.range[1])*24*3600
+        left, right = start - float(opt.range[0])*24*3600, start - float(opt.range[1])*24*3600
 
         res = await ValorSQL._execute(f"SELECT * FROM terr_count WHERE guild IN ({','.join(g_names_paren)}) AND time >= {left} AND time <= {right} ORDER BY time") # time, guild, count
 
@@ -53,7 +53,7 @@ async def _register_wipe(valor: Valor):
                 last_time[guild] = t_time
                 wipe_time[guild] += (t_time - left) / 3600 # left end of the data
             
-            if count <= opt.threshold and (t_time - last_time[guild]) >= opt.minsec+0.5:
+            if float(opt.threshold[1]) <= count <= float(opt.threshold[0]) and (t_time - last_time[guild]) >= opt.minsec+0.5:
                 wipe_time[guild] += (t_time - last_time[guild]) / 3600
                 if not guild in wiped_count:
                     wiped_count[guild] = 0
