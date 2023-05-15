961b7aeeddb9607ab5ccda90303cff7f651433c3
classAndrew <classAndrew@users.noreply.github.com>
2023-05-15 14:47:23 -0700

    use deepcopy in tables.fmt

--- util/tables.py
+++ util/tables.py
@@ -4,10 +4,11 @@ def fmt(header: List[str], rows: List[List[any]], separators=False, str_converte
     # go through each column and find what the longest one is
     str_converter = str_converter if str_converter else str
     max_lengths = [len(h) for h in header]
+    rows_copy = [list(r) for r in rows]
 
     for r in range(len(rows)):
         for c in range(len(rows[r])):
-            rows[r][c] = str_converter(rows[r][c])
+            rows_copy[r][c] = str_converter(rows[r][c])
             max_lengths[c] = max(max_lengths[c], len(rows[r][c]))
     
     header_str = ' | '.join(f'%{max_lengths[i]}s' % header[i] for i in range(len(max_lengths)))
@@ -21,7 +22,7 @@ def fmt(header: List[str], rows: List[List[any]], separators=False, str_converte
             line_separators += '-'   
 
     body = ''
-    for r in rows:
+    for r in rows_copy:
         body += ' | '.join(f'%{max_lengths[i]}s' % r[i] for i in range(len(max_lengths))) + '\n'
         if separators:
             body += line_separators + '\n'
