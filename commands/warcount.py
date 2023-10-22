914a0a42f9379b3d5d5d9a30857b4582219e591f
classAndrew <classAndrew@users.noreply.github.com>
2023-10-22 15:28:21 -0700

    add global / local ranking to warcount

--- commands/warcount.py
+++ commands/warcount.py
@@ -14,16 +14,16 @@ import argparse
 load_dotenv()
 async def _register_warcount(valor: Valor):
     desc = "Gets you the war count leaderboard. (old version)"
-    clone_map = {"hunter": "Archer", "knight": "Warrior", "darkwizard": "Mage", "ninja": "Assassin", "skyseer": "Shaman"}
+    clone_map = {"HUNTER": "ARCHER", "KNIGHT": "WARRIOR", "DARKWIZARD": "MAGE", "NINJA": "ASSASSIN", "SKYSEER": "SHAMAN"}
+    clone_map_inv = {clone_map[k]: k for k in clone_map}
     real_classes = clone_map.values()
-    # season_times={8:[1663016400,1666569600],9:[1666983600,1672462800]} # ! idk the end time for season 9 so i put a big number
     parser = argparse.ArgumentParser(description='Warcount Command')
     parser.add_argument('-n', '--names', nargs='+', default=[])
     parser.add_argument('-g', '--guild', nargs='+', default=[]) # this one is filter players only in guilds, Callum: 100
     parser.add_argument('-gs', '--guildsum', nargs='+', default=[]) # TODO: this one is for guild totals ANO: 100, ESI: 200 etc.
     parser.add_argument('-c', '--classes', nargs='+', default=[])
     parser.add_argument('-r', '--range', nargs='+', default=None)
-    parser.add_argument('-rk', '--rank', action='store_true')
+    parser.add_argument('-rk', '--rank', type=str, default="global")
 
     @valor.command()
     async def warcount(ctx: Context, *options):
@@ -33,11 +33,36 @@ async def _register_warcount(valor: Valor):
             return await LongTextEmbed.send_message(valor, ctx, "warcount", parser.format_help().replace("main.py", "-warcount"), color=0xFF00)
     
         listed_classes = real_classes if not opt.classes else opt.classes
-        listed_classes_enumerated = {v.lower(): i for i, v in enumerate(listed_classes)} # {classname: 0, classname: 1, ...}
+        listed_classes = [x.upper() for x in listed_classes]
+        listed_classes_enumerated = {v.upper(): i for i, v in enumerate(listed_classes)} # {classname: 0, classname: 1, ...}
 
-        names = {n.lower() for n in opt.names} if opt.names else None
+        names = {n.upper() for n in opt.names} if opt.names else None
 
         start = time.time()
+
+        table_type = "cumu_warcounts" if not opt.range else "delta_warcounts"
+        table_count_column = "warcount" if not opt.range else "warcount_diff"
+
+        warcount_query = f'''SELECT uuid_name.name, 
+    %s,
+	SUM({table_type}.{table_count_column}) as all_wars, 
+	player_stats.guild
+FROM {table_type} 
+LEFT JOIN uuid_name ON uuid_name.uuid={table_type}.uuid 
+LEFT JOIN player_stats ON player_stats.uuid={table_type}.uuid 
+WHERE UPPER({table_type}.class_type) IN (%s) %%s 
+GROUP BY uuid_name.uuid, player_stats.guild
+ORDER BY all_wars DESC;'''
+        
+
+        class_column_count_parts = []
+        select_class_in_parts = []
+        for real_class in listed_classes:
+            class_column_count_parts.append( # lol the dict lookup will fail if user tries some tricky sql input
+                f"SUM(CASE WHEN UPPER({table_type}.class_type)='{real_class}' OR UPPER({table_type}.class_type)='{clone_map_inv[real_class]}' THEN {table_type}.{table_count_column} ELSE 0 END) AS {real_class}_count")
+            select_class_in_parts.append(f"'{real_class}', '{clone_map_inv[real_class]}'")
+        
+        warcount_query = warcount_query % (','.join(class_column_count_parts), ','.join(select_class_in_parts))
 
         if opt.range:
             # opt.range = [2e9, 0]
@@ -46,41 +71,35 @@ async def _register_warcount(valor: Valor):
                 return await ctx.send(embed=ErrorEmbed("Invalid season name input"))
             left, right = valid_range
 
-            res = await ValorSQL._execute(f'''SELECT uuid_name.name, delta_warcounts.warcount_diff, delta_warcounts.class_type, player_stats.guild
-FROM delta_warcounts 
-LEFT JOIN uuid_name ON uuid_name.uuid=delta_warcounts.uuid 
-LEFT JOIN player_stats ON player_stats.uuid=delta_warcounts.uuid WHERE delta_warcounts.time >= {left} AND delta_warcounts.time <= {right} ORDER BY delta_warcounts.time ASC;''')
+            res = await ValorSQL._execute(warcount_query % f' AND delta_warcounts.time >= {left} AND delta_warcounts.time <= {right}')
         else:
-            res = await ValorSQL._execute(f'''SELECT uuid_name.name, cumu_warcounts.warcount, cumu_warcounts.class_type, player_stats.guild
-FROM cumu_warcounts 
-LEFT JOIN uuid_name ON uuid_name.uuid=cumu_warcounts.uuid 
-LEFT JOIN player_stats ON player_stats.uuid=cumu_warcounts.uuid''')
+            res = await ValorSQL._execute(warcount_query % '')
+
         delta_time = time.time()-start
         
         guild_names, unidentified = await guild_names_from_tags(opt.guild)
 
-        header = [' '*14+"Name", "Guild", *[f"  {x}  " for x in listed_classes], "  Total  "]
-        if opt.rank:
-            header = ["  Rank  "] + [' '*14+"Name", "Guild", *[f"  {x}  " for x in listed_classes], "  Total  "]
+        header = ['  Rank  ', ' '*14+"Name", "Guild", *[f"  {x}  " for x in listed_classes], "  Total  "]
 
         player_to_guild = {}
         guilds_seen = set()
         player_warcounts = {}
 
-        for name, wardiff, class_type, guild in res:
+        name_to_ranking = {}
+
+        for rank_0, row in enumerate(res):
+            name, total, guild = row[0], row[-2], row[-1]
+            name_to_ranking[name] = rank_0+1
+            classes_count = row[1:-2]
+
             if opt.guild and not guild in guild_names: continue
-            if not name or (opt.names and not name.lower() in names): continue
-
-            if not class_type: continue # skip. errors in early db insertions
-            class_type = class_type.lower()
-            real_class = clone_map.get(class_type, class_type).lower()
-
-            if not real_class in listed_classes_enumerated: continue
-
+            if not name or (opt.names and not name.upper() in names): continue
             if not name in player_warcounts:
                 player_warcounts[name] = [0]*len(listed_classes_enumerated)
 
-            player_warcounts[name][listed_classes_enumerated[real_class]] += wardiff
+            for i, real_class in enumerate(listed_classes):
+                player_warcounts[name][listed_classes_enumerated[real_class]] += classes_count[i]
+
             player_to_guild[name] = guild
             guilds_seen.add(guild)
         
@@ -92,10 +111,16 @@ LEFT JOIN player_stats ON player_stats.uuid=cumu_warcounts.uuid''')
                 if priority > guild_to_tag.get(guild, ("N/A", -1))[1]:
                     guild_to_tag[guild] = (tag, priority)
 
-        if opt.rank:
-            pass
-        rows = [(name, guild_to_tag.get(player_to_guild[name], ("None", -1))[0], *player_warcounts[name], sum(player_warcounts[name])) for name in player_warcounts]
-        rows.sort(key=lambda x: x[-1], reverse=True) 
+
+        if opt.rank != "global":
+            rows_total_count = [(name, sum(player_warcounts[name])) for name in player_warcounts]
+            rows_total_count.sort(key=lambda x: x[-1], reverse=True) 
+            name_to_ranking = {}
+            for i, rest in enumerate(rows_total_count):
+                name_to_ranking[rest[0]] = i+1
+        
+        rows = [(name_to_ranking[name], name, guild_to_tag.get(player_to_guild[name], ("None", -1))[0], *player_warcounts[name], sum(player_warcounts[name])) for name in player_warcounts]
+        rows.sort(key=lambda x: x[-1], reverse=True)
 
         opt_after = f"\nQuery took {delta_time:.3}s. Requested at {datetime.utcnow().ctime()}"
         await LongTextTable.send_message(valor, ctx, header, rows, opt_after)
