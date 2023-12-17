c87e500f668eabd7ae61a3c9efcbecf3d7fb4a73
classAndrew <classAndrew@users.noreply.github.com>
2023-12-17 10:22:50 -0800

    use default group and add argparse to online command

--- commands/online.py
+++ commands/online.py
@@ -3,23 +3,24 @@ from discord.ext.commands import Context
 from util import ErrorEmbed, LongTextEmbed, LongFieldEmbed
 from .common import guild_name_from_tag
 import random
+import argparse
 import requests
 
 async def _register_online(valor: Valor):
-    desc = "Online: shows who's online in a guild"
+    desc = "Online: shows who's online in a guild\nThe new command is formatted like this: -online -g <guild_TAG>.\n For example: -online -g ano"
     rank_order = dict(zip(["recruit", "recruiter", "captain", "strategist", "chief", "owner"], range(5, -1, -1)))
-    @valor.command()
-    async def online(ctx: Context, *args, guild="Titans Valor"):
-        group = False
-        if args and args[0] == 'g':
-            args = args[1:]
-            group = True
-        guild = ' '.join(args) if len(args) else guild
-        res = requests.get(valor.endpoints["guild"].format(guild)).json()
 
-        if "members" not in res or not res["name"]:
-            # try using the tag instead
-            guild = await guild_name_from_tag(guild)
+    parser = argparse.ArgumentParser(description='Online command')
+    parser.add_argument('-g', '--guild', type=str)
+    
+    @valor.command()
+    async def online(ctx: Context, *options):
+        try:
+            opt = parser.parse_args(options)
+        except:
+            return await LongTextEmbed.send_message(valor, ctx, "Online", parser.format_help().replace("main.py", "-online"), color=0xFF00)
+        
+        guild = await guild_name_from_tag(opt.guild)
 
         res = requests.get(valor.endpoints["guild"].format(guild)).json()
 
@@ -32,29 +33,24 @@ async def _register_online(valor: Valor):
         
         if not len(online_rn):
             return await LongTextEmbed.send_message(valor, ctx, f"{guild} Members Online", "There are no members online.", color = 0xFF)
-        if group:
-            grouped = {}
-            for p, k, rank in online_rn:
-                if not k in grouped:
-                    grouped[k] = []
-                grouped[k].append((p, k, rank))
-                grouped[k].sort(key=lambda x: rank_order[x[2]])
-            grouped = [x[1] for x in sorted(grouped.items(), key=lambda v: len(v[1]), reverse=True)]
-            return await LongTextEmbed.send_message(valor, ctx, f"Members of {guild} online ({len(online_rn)})", '```' +
-                '-'*17+'+'+'-'*7+'+'+'-'*12+'\n'+
-                '\n'.join('\n'.join(('%16s | %5s | %10s' % x) for x in y)+'\n' for y in grouped) + '```',
-                color=0xa1ffe1    
-            )
-        online_rn.sort(key = lambda x: rank_order[x[2]])
-        # escape any underscores and concat rank with world number
-        online_rn = [(p.replace("_", "\_"), f"{k}\n{rank}") for p, k, rank in online_rn]
 
-        await LongFieldEmbed.send_message(valor, ctx, f"{guild} Members Online ({len(online_rn)})", online_rn)
-        # await ctx.send("```"+'\n'.join("%16s | %8s" % (p, k) for p, k in online_rn) + "```")
+        grouped = {}
+        for p, k, rank in online_rn:
+            if not k in grouped:
+                grouped[k] = []
+            grouped[k].append((p, k, rank))
+            grouped[k].sort(key=lambda x: rank_order[x[2]])
+        grouped = [x[1] for x in sorted(grouped.items(), key=lambda v: len(v[1]), reverse=True)]
+        return await LongTextEmbed.send_message(valor, ctx, f"Members of {guild} online ({len(online_rn)})", '```' +
+            '-'*17+'+'+'-'*7+'+'+'-'*12+'\n'+
+            '\n'.join('\n'.join(('%16s | %5s | %10s' % x) for x in y)+'\n' for y in grouped) + '```',
+            color=0xa1ffe1    
+        )
+    
     
     @online.error
     async def cmd_error(ctx, error: Exception):
-        await ctx.send(embed=ErrorEmbed())
+        await ctx.send(embed=ErrorEmbed("The new command is formatted like this: -online -g <guild_TAG>.\n For example: -online -g ano"))
         print(error.with_traceback())
     
     @valor.help_override.command()
