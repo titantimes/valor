c4adb5c6d0217f144c5c8d604ce7a1b902d6fb0f
classAndrew <classAndrew@users.noreply.github.com>
2024-10-07 21:57:53 -0700

    add graids guild_wise and rank order

--- commands/graids.py
+++ commands/graids.py
@@ -5,7 +5,7 @@ from util import ErrorEmbed, HelpEmbed, LongFieldEmbed, LongTextEmbed, LongTextT
 from discord.ext.commands import Context
 from datetime import datetime
 from dotenv import load_dotenv
-from .common import get_left_right
+from .common import get_left_right, guild_names_from_tags
 import argparse
 
 load_dotenv()
@@ -14,7 +14,7 @@ async def _register_graids(valor: Valor):
     parser = argparse.ArgumentParser(description='Warcount Command')
     parser.add_argument('-r', '--range', nargs='+', default=None)
     parser.add_argument('-g', '--guild', nargs='+', default=None)
-
+    parser.add_argument('-w', '--guild_wise', action="store_true", default=False)
 
     @valor.command()
     async def graids(ctx: Context, *options):
@@ -25,18 +25,34 @@ async def _register_graids(valor: Valor):
     
         start = time.time()
 
-        template_query = '''
-SELECT B.name, SUM(A.num_raids) AS raid_cnt
+        if not opt.guild_wise:
+            template_query = '''
+SELECT ROW_NUMBER() OVER(ORDER BY raid_cnt DESC) AS `raid_cnt`, name, raid_cnt
 FROM
-	guild_raid_records A LEFT JOIN uuid_name B ON A.uuid=B.uuid
-WHERE 
-    {TIME_CLAUSE}
-    AND A.guild IN ("Titans Valor")
-GROUP BY A.uuid
-ORDER BY raid_cnt DESC;
+    (SELECT B.name, SUM(A.num_raids) AS raid_cnt
+    FROM
+        guild_raid_records A LEFT JOIN uuid_name B ON A.uuid=B.uuid
+    WHERE 
+        {TIME_CLAUSE}
+        {GUILD_CLAUSE}
+    GROUP BY A.uuid
+    ORDER BY raid_cnt DESC) C;
+''' 
+        else:
+            template_query = '''
+SELECT ROW_NUMBER() OVER(ORDER BY raid_cnt DESC), guild, raid_cnt
+FROM
+    (SELECT guild, SUM(num_raids) AS raid_cnt
+    FROM
+        guild_raid_records A
+    WHERE
+        {TIME_CLAUSE}
+        {GUILD_CLAUSE}
+    GROUP BY guild
+    ORDER BY raid_cnt DESC) C;
 '''
 
-        template_query_params = { "TIME_CLAUSE": "" }
+        template_query_params = { "TIME_CLAUSE": "", "GUILD_CLAUSE": "" }
         prepared_params = []
 
         if not opt.range:
@@ -50,6 +66,14 @@ ORDER BY raid_cnt DESC;
         template_query_params["TIME_CLAUSE"] = "A.`time` > %s AND A.`time` <= %s"
         prepared_params.append(left)
         prepared_params.append(right)
+
+        if not opt.guild_wise and not opt.guild:
+            opt.guild = ["ANO"]
+        
+        if opt.guild:
+            opt.guild, _ = await guild_names_from_tags(opt.guild) # second arg for errors. do later
+            template_query_params["GUILD_CLAUSE"] = "AND A.guild IN (" + ("%s,"*len(opt.guild))[:-1] + ")"
+            prepared_params.extend(opt.guild)
         
         query = template_query.format(**template_query_params)
         
@@ -57,7 +81,7 @@ ORDER BY raid_cnt DESC;
 
         delta_time = time.time()-start
         
-        header = [' '*14+"Name", "  Total  "]
+        header = [' Rank ', ' '*14+"Name", "  Total  "]
         
         # rows = [(name_to_ranking[name], name, guild_to_tag.get(player_to_guild[name], ("None", -1))[0], *player_warcounts[name], sum(player_warcounts[name])) for name in player_warcounts]
         # rows.sort(key=lambda x: x[-1], reverse=True)
