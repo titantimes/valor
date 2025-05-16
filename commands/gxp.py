8fc7fe7f44f8db84ea55ddd174508e30be7e1b16
C0rupted <kanjpatel33@gmail.com>
2025-05-16 17:05:38 +1000

    GXP command fancy-fication

--- commands/gxp.py
+++ commands/gxp.py
@@ -5,12 +5,169 @@ from sql import ValorSQL
 from discord.ext.commands import Context
 from datetime import datetime
 from dotenv import load_dotenv
-import os
-import time
+import discord, asyncio, time, os, aiohttp, math
+from discord.ui import View
+from discord import File
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 load_dotenv()
 async def _register_gxp(valor: Valor):
     desc = "Gets you GXP of a member. `-gxp h` to get the xp contributions of a player with time slices"
+    model_base = "https://visage.surgeplay.com/bust/"
+
+    def basic_table(data: list[tuple], page: int, title: str) -> str:
+        per_page = 20
+        start = page * per_page
+        end = start + per_page
+        sliced = data[start:end]
+
+        i = 1+(page*per_page)
+
+        table = f"```isbl\n{title}\n\n"
+        table += " Rank ┃ Name            ┃ XP             \n"
+        table += "━━━━━━╋━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━\n"
+
+        for player in sliced:
+            rank = f"{i})"
+            rank_pad = " " * int((6 - len(rank)))
+            name = player[0]
+            name_pad = " " * int((16 - len(name)))
+            xp = f"{int(player[1]):,}"
+            xp_pad = " " * int((16 - len(xp)))
+
+            t = f"{rank}{rank_pad}┃ {name}{name_pad}┃ {xp}{xp_pad}\n"
+            table += t
+            i += 1
+
+        table += "━━━━━━╋━━━━━━━━━━━━━━━━━╋━━━━━━━━━━━━━━━━\n```"
+
+        return table
+
+
+    async def download_model(session, url, filename):
+        user_agent = {'User-Agent': 'valor-bot/1.0'}
+        try:
+            async with session.get(url, headers=user_agent) as response:
+                if response.status == 200:
+                    content = await response.read()
+                    with open(filename, "wb") as f:
+                        f.write(content)
+                else:
+                    print(f"Failed to fetch {url}: {response.status}")
+        except Exception as e:
+            print(f"Error fetching {url}: {e}")
+
+    async def fetch_all_models(rows):
+        tasks = []
+        now = time.time()
+        async with aiohttp.ClientSession() as session:
+            for row in rows:
+                if row[0]:
+                    filename = f"/tmp/{row[0]}_model.png"
+                    url = model_base + row[0] + '.png'
+
+                    if not os.path.exists(filename) or now - os.path.getmtime(filename) > 24 * 3600:
+                        tasks.append(download_model(session, url, filename))
+            await asyncio.gather(*tasks)  # Run all downloads in parallel
+
+
+    async def fancy_table(data: list[tuple], page: int):
+        start = page * 10
+        end = start + 10
+        sliced = data[start:end]
+
+        rank_margin = 40
+        model_margin = 110
+        name_margin = 200
+        value_margin = 680
+
+        font = ImageFont.truetype("MinecraftRegular.ttf", 20)
+        board = Image.new("RGBA", (720, 730), (255, 0, 0, 0))
+        overlay = Image.open("assets/overlay.png")
+        draw = ImageDraw.Draw(board)
+
+        await fetch_all_models(sliced)
+
+        i = 1
+        rank = 1+(page*10)
+        for stat in sliced:
+
+            height = (i*74)-74
+            board.paste(overlay, (0, height))
+            match rank:
+                case 1:
+                    color = "yellow"
+                case 2:
+                    color = (170,169,173,255)
+                case 3:
+                    color = (169,113,66,255)
+                case _:
+                    color = "white"
+            try:
+                model_img = Image.open(f"/tmp/{stat[0]}_model.png", 'r')
+                model_img = model_img.resize((64, 64))
+            except Exception as e:
+                model_img = Image.open(f"assets/unknown_model.png", 'r')
+                model_img = model_img.resize((64, 64))
+                print(f"Error loading image: {e}")
+
+            board.paste(model_img, (model_margin, height), model_img)
+            draw.text((rank_margin, height+22), "#"+str(rank), fill=color, font=font)
+            draw.text((name_margin, height+22), str(stat[0]), font=font)
+            draw.text((value_margin, height+22), f"{int(stat[1]):,}", font=font, anchor="rt")
+
+            rank += 1
+            i += 1
+
+
+        board.save("/tmp/gxp.png")
+
+        return File("/tmp/gxp.png", filename="gxp.png")
+
+    class GXPView(View):
+        def __init__(self, ctx, rows, title, timeout=60):
+            super().__init__(timeout=timeout)
+            self.ctx = ctx
+            self.is_fancy = False
+
+            self.page = 0
+            self.title = title
+            self.data = rows
+            
+            self.max_pages = math.ceil(len(rows) / 10)
+
+        async def update_message(self, interaction: discord.Interaction):
+            if self.is_fancy:
+                await interaction.response.defer()
+                content = await fancy_table(self.data, self.page)
+                await interaction.edit_original_response(content="", view=self, attachments=[content])
+            else:
+                content = basic_table(self.data, self.page, self.title)
+                await interaction.response.edit_message(content=content, view=self, attachments=[])
+
+        @discord.ui.button(label="⬅️")
+        async def previous(self, interaction: discord.Interaction, button: discord.ui.Button):
+            if self.page > 0:
+                self.page -= 1
+                await self.update_message(interaction)
+            else:
+                await interaction.response.defer()
+
+        @discord.ui.button(label="➡️")
+        async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+            if self.page < self.max_pages - 1:
+                self.page += 1
+                await self.update_message(interaction)
+            else:
+                await interaction.response.defer()
+        
+        @discord.ui.button(label="✨")
+        async def fancy(self, interaction: discord.Interaction, button: discord.ui.Button):
+            self.is_fancy = not self.is_fancy
+            await self.update_message(interaction)
+
 
     @valor.group()
     async def gxp(ctx: Context, guild="Titans Valor", player="", arg2 = "", arg3 = ""):
@@ -74,12 +231,16 @@ ORDER BY `B`.`value`  DESC
 """, (guild))
             
         # if isinstance(res, tuple):
-        mesg = [[k[0], k[1]] for k in res]
-        mesg = sorted(mesg, key=lambda x: x[1], reverse=True)
-        total = sum(x[1] for x in mesg)
-        for x in mesg:
-            x[1] = f"{x[1]:,}"
-        await LongFieldEmbed.send_message(valor, ctx, f"{guild} XP Breakdown (Total: {total:,})", mesg)
+        total = 0
+        for value in res:
+            total += value[1]
+        total = int(total)
+
+        title = f"{guild} — XP Breakdown (Total: {total:,})"
+
+        view = GXPView(ctx, res, title)
+
+        await ctx.send(content=basic_table(res, 0, title), view=view)
         # else:
         #     uuid = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{player}").json()["id"]
         #     # add hyphens
